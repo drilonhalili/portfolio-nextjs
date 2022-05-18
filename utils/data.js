a66b05afd5a416109c7fff92edc23ec15f915698
Drilon Halili <drilon.halili@gmail.com>
2022-05-18 14:25:15 +0200

    feat(assets): Add base files

---
+++ utils/data.js
@@ -0,0 +1,99 @@
+export const projects = [
+  {
+    projectName: "My dashboard",
+    webLink: "",
+    githubLink: "https://github.com/drilonhalili/my-dashboard",
+    description: `Dashboard project that provides CRUD of products, manage the user profile, etc. `,
+    technicalDetails:
+      "REACT - REDUX - REACT QUERY - FIREBASE - FORMIK - ANT DESIGN - SASS",
+  },
+  {
+    projectName: "Video Chat App",
+    webLink: "",
+    githubLink: "https://github.com/drilonhalili/video-chat-app",
+    description: `Video chat one ore more participants, share their screen, login form, etc.`,
+    technicalDetails:
+      "NEXT JS - TWILIO - FIREBASE - SASS - ANT DESIGN - TYPESCRIPT",
+  },
+  {
+    projectName: "My portfolio",
+    webLink: "https://drilonhalili.io",
+    githubLink: "https://github.com/drilonhalili/my-portfolio",
+    description: `Portfolio project that tells about me and my projects.`,
+    technicalDetails: "NEXT JS - SASS - ANT DESIGN - TYPESCRIPT",
+  },
+  {
+    projectName: "Ishop",
+    webLink: "https://ishoptetove.com",
+    githubLink: "https://github.com/drilonhalili/ishop",
+    description: ``,
+    technicalDetails: "",
+  },
+]
+
+export const socials = [
+  {
+    name: "twitter",
+    socialLink: "https://www.twitter.com/drilonhalili12",
+    icon: "twitter",
+  },
+  {
+    name: "facebook",
+    socialLink: "https://www.facebook.com/drilon.halili",
+    icon: "facebook",
+  },
+  {
+    name: "github",
+    socialLink: "https://www.github.com/drilonhalili",
+    icon: "github",
+  },
+  {
+    name: "linkedin",
+    socialLink: "https://www.linkedin.com/in/drilonhalili/",
+    icon: "linkedin",
+  },
+  {
+    name: "instagram",
+    socialLink: "https://www.instagram.com/drilon.halili/",
+    icon: "instagram",
+  },
+]
+
+export const skills = [
+  {
+    name: "HTML",
+    image: "html.png",
+  },
+  {
+    name: "CSS",
+    image: "css.png",
+  },
+  {
+    name: "JavaScript",
+    image: "javascript.png",
+  },
+  {
+    name: "React",
+    image: "react.png",
+  },
+  {
+    name: "Tailwind",
+    image: "tailwind.png",
+  },
+  {
+    name: "Firebase",
+    image: "firebase.png",
+  },
+  {
+    name: "Github",
+    image: "github1.png",
+  },
+  {
+    name: "Shopify",
+    image: "shopify.png",
+  },
+  {
+    name: "Twillio",
+    image: "twillio.png",
+  },
+]
