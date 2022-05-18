1c71c7cc9b3962862a4e8bc0351edc95aa0b89bb
Drilon Halili <drilon.halili@gmail.com>
2022-05-18 15:52:48 +0200

    feat(Contact): Make static iamges

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from "next/image"
+import pc from '../public/assets/pc.jpg'
 
 const About = () => {
   return (
@@ -26,10 +28,12 @@ const About = () => {
           </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <img
+          <Image
             className="rounded-xl"
-            src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80"
+            src={pc}
             alt="/"
+            height={500}
+            width={500}
           />
         </div>
       </div>
--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,11 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import {HiOutlineChevronDoubleUp} from 'react-icons/hi'
+import getInTouch from '../public/assets/getInTouch.jpg'
+
 import { socials } from "../utils/data"
+
 import SocialAvatar from "./SocialAvatar"
 
 const Contact = () => {
@@ -17,10 +21,12 @@ const Contact = () => {
           <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
             <div className="lg:p-4 h-full ">
               <div>
-                <img
+                <Image
                   className="rounded-xl hover:scale-105 ease-in duration-300"
-                  src="https://images.unsplash.com/photo-1516387938699-a93567ec168e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80"
+                  src={getInTouch}
                   alt="/"
+                  width={1000}
+                  height={600}
                 />
               </div>
               <div>
@@ -39,6 +45,7 @@ const Contact = () => {
                       key={link.name}
                       href={link.socialLink}
                       name={link.name}
+                      className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300"
                     />
                   ))}
                 </div>
