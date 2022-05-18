a0ad4c12130264bde5346fb1081177acb53f7af5
Drilon Halili <drilon.halili@gmail.com>
2022-05-18 14:48:25 +0200

    feat(Skills): Create skills component

---
+++ components/Skills.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Testimonial from './Testimonial';
+
+import { skills } from '../utils/data';
+
+const Skills = () => {
+  return (
+    <div id="skills" className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {skills.map((skill) => (
+            <Testimonial
+              key={skill.name}
+              name={skill.name}
+              src={`/../public/assets/skills/${skill.image}`}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Skills;
