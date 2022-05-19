a2229e5bcdbc4fb5fe6f1e45a4e5fb9579b5e26d
Drilon Halili <drilon.halili@gmail.com>
2022-05-19 09:56:55 +0200

    feat(analytics): Add analytics for page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import Navbar from '../components/Navbar';
+import { usePanelbear } from '../lib/analytics';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  usePanelbear(process.env.NEXT_PUBLIC_FATHOM_SITE_ID)
   return (
     <>
       <Navbar />
